18885088dde6606e08e49894e1d8944fcbc0f8c3
Reece Holmdahl <3149587+reeceholmdahl@users.noreply.github.com>
2021-08-24 19:23:30 -0500

    Staggering requests to bestbuy's stock

--- monitor.js
+++ monitor.js
@@ -48,7 +48,7 @@ async function checkStock() {
     headers.append('Connection', 'keep-alive');
     headers.append('Host', 'www.bestbuy.com');
 
-    for (const store of AppState.monitorData.stores) {
+    for (const [index, store] of AppState.monitorData.stores.entries()) {
 
         // Set user agent per call? might assure my calls get thru to other stores indepedently idk
         headers.set('User-Agent', `Monitor/${AppState.monitorData.version} API Monitor of ${store.name}/${store.id}`)
@@ -57,44 +57,47 @@ async function checkStock() {
         const url = `https://www.bestbuy.com/api/tcfb/model.json?paths=%5B%5B%22shop%22%2C%22buttonstate%22%2C%22v5%22%2C%22item%22%2C%22consolidated%22%2C%22skus%22%2C%5B${skuInsert}%5D%2C%22conditions%22%2C%22NONE%22%2C%22destinationZipCode%22%2C${store.zip}%2C%22storeId%22%2C${store.id}%2C%22context%22%2C%5B%22%2520%22%2C%22package-deals-carousel%22%5D%2C%22addAll%22%2C%22false%22%2C%22consolidated%22%2C%22true%22%2C%22consolidatedButtonState%22%2C%5B%22buttonState%22%2C%22displayText%22%5D%5D%5D&method=get`
 
         // Make api call
-        const response = await fetch(url, {
-            headers
-        });
+        setTimeout(function() {
+            fetch(url, {
+                headers
+            })
+                .then(response => response.json())
+                .then(json => {
+                    // log store to console
+                    console.log(`STORE: ${store.name}:`);
 
-        // console.log(response);
+                    for (const sku of AppState.monitorData.skus) {
+                        
+                        const buttonValue = json.jsonGraph.shop.buttonstate.v5.item.consolidated.skus[sku].conditions.NONE.destinationZipCode[store.zip].storeId[store.id].context["%20"].addAll.false.consolidated.true.consolidatedButtonState.buttonState.value;
+                        
+                        const maybeAbleToBuy = buttonValue !== CANT_BUY;
 
-        // parse json from call
-        const json = await response.json();
+                        console.log(`SKU ${sku}: ${maybeAbleToBuy ? 'may be able to buy!' : 'can\'t buy'} (${buttonValue}) @ https://www.bestbuy.com/site/${sku}.p`);
+                        
+                        if (maybeAbleToBuy) {
+                            const message = {
+                                from: EMAIL_SENDER,
+                                to: EMAIL_RECIPIENT,
+                                subject: `Critical Alert, ${sku} in Stock`,
+                                text: 
+                                `https://www.bestbuy.com/site/${sku}.p\nat ${store.name} near ${store.zip} @ #${store.id}; TIME: ${Date.now()}`,
+                            }
 
-        // log store to console
-        console.log(`STORE: ${store.name}:`);
+                            mailer.sendMail(message, (err, info) => {
+                                if (err) console.log(err);
+                                // else console.log(info);
+                            });
+                            AppState.lastEmailTime = Date.now();
+                        }
+                    }
 
-        for (const sku of AppState.monitorData.skus) {
-            
-            const buttonValue = json.jsonGraph.shop.buttonstate.v5.item.consolidated.skus[sku].conditions.NONE.destinationZipCode[store.zip].storeId[store.id].context["%20"].addAll.false.consolidated.true.consolidatedButtonState.buttonState.value;
-            
-            const maybeAbleToBuy = buttonValue !== CANT_BUY;
-
-            console.log(`SKU ${sku}: ${maybeAbleToBuy ? 'may be able to buy!' : 'can\'t buy'} (${buttonValue}) @ https://www.bestbuy.com/site/${sku}.p`);
-            
-            if (maybeAbleToBuy) {
-                const message = {
-                    from: EMAIL_SENDER,
-                    to: EMAIL_RECIPIENT,
-                    subject: `Critical Alert, ${sku} in Stock`,
-                    text: 
-                    `https://www.bestbuy.com/site/${sku}.p\nat ${store.name} near ${store.zip} @ #${store.id}; TIME: ${Date.now()}`,
-                }
-
-                mailer.sendMail(message, (err, info) => {
-                    if (err) console.log(err);
-                    // else console.log(info);
+                    console.log();
+                })
+                .catch(rejection => {
+                    console.error(`Issue with request to store #${store.id}, ${store.zip}; ${store.name}:`)
+                    console.error(rejection);
                 });
-                AppState.lastEmailTime = Date.now();
-            }
-        }
-
-        console.log();
+        }, Math.random() * index * 250);
     }
 }
 
